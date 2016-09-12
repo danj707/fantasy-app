640190af9f0df20dd8f360a06dc0421c1a75918c
dan <danjenner@gmail.com>
2016-09-12 17:48:53 +0000

    Updated port listener, moved favicon.ico

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/fantasy-app' :
                             'mongodb://localhost/fantasy-app-dev');
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 5000;
